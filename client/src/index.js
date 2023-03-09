6ace6d62891f8f5f51db43d213d4115d6a1230b6
andrea <andrea.buldorini2013@gmail.com>
2023-03-09 18:56:58 -0300

    modificaciones

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import store from "./redux/store";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import axios from "axios"
-axios.defaults.baseURL = 'https://deploy-production-208a.up.railway.app/';
+axios.defaults.baseURL = 'https://deploy-production-275f.up.railway.app/';
 
 ReactDOM.render(
     <Provider store={store}>  
